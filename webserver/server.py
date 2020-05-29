56df6fff6a0d1826632d94868897f82acbd96d4c
Andrei Draghiceanu <andrei.draghiceanu@gmail.com>
2020-05-29 12:53:36 +0300

    Updated website.

--- webserver/server.py
+++ webserver/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
 app = Flask(__name__)
 
 
@@ -7,31 +9,28 @@ def my_home():
     return render_template('/index.html')
 
 
-@app.route('/index')
-def index():
-    return render_template('/index.html')
+@app.route('/<string:page_name>')
+def index(page_name):
+    return render_template(page_name)
 
 
-@app.route('/about')
-def about():
-    return render_template('/about.html')
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
 
-@app.route('/components')
-def components():
-    return render_template('/components.html')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('/contact.html')
-
-
-@app.route('/work')
-def work():
-    return render_template('/work.html')
-
-
-@app.route('/works')
-def works():
-    return render_template('/works.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except Exception as err:
+            return 'did not save to database \n', err
+    else:
+        return 'something went wrong. Try again!'
